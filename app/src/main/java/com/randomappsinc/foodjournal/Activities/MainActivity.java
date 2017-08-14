583d7f31325e6d2e230d50cbb0a81a075a1c7c41
Gear61 <chessnone@yahoo.com>
2017-08-13 19:29:29 -0700

    Add FAB menu

--- app/src/main/java/com/randomappsinc/foodjournal/Activities/MainActivity.java
+++ app/src/main/java/com/randomappsinc/foodjournal/Activities/MainActivity.java
@@ -2,9 +2,6 @@ package com.randomappsinc.foodjournal.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,44 +10,45 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+import com.joanzapata.iconify.IconDrawable;
+import com.joanzapata.iconify.fonts.IoniconsIcons;
 import com.randomappsinc.foodjournal.Adapters.IconItemsAdapter;
 import com.randomappsinc.foodjournal.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.OnItemClick;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.nav_options) ListView mNavOptions;
+    @BindView(R.id.toolbar) Toolbar mToolbar;
     @BindView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.pick_source) FloatingActionMenu mSourcePicker;
+    @BindView(R.id.from_camera) FloatingActionButton mCameraPicker;
+    @BindView(R.id.from_files) FloatingActionButton mFilesPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-
         mNavOptions.setAdapter(new IconItemsAdapter(this, R.array.nav_drawer_tabs, R.array.nav_drawer_icons));
+
+        mCameraPicker.setImageDrawable(new IconDrawable(this,
+                IoniconsIcons.ion_android_camera).colorRes(R.color.white));
+        mFilesPicker.setImageDrawable(new IconDrawable(this,
+                IoniconsIcons.ion_android_folder).colorRes(R.color.white));
     }
 
     @OnItemClick(R.id.nav_options)
@@ -64,6 +62,11 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         startActivity(intent);
+    }
+
+    @OnClick({R.id.from_camera, R.id.from_files})
+    public void startRace(View view) {
+        mSourcePicker.close(true);
     }
 
     @Override
