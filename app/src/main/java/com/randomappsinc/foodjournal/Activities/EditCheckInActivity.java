ce4309bc7965308d9d95da3de745607770a1ca55
Gear61 <chessnone@yahoo.com>
2017-08-30 22:56:27 -0700

    Edit check-in

--- app/src/main/java/com/randomappsinc/foodjournal/Activities/EditCheckInActivity.java
+++ app/src/main/java/com/randomappsinc/foodjournal/Activities/EditCheckInActivity.java
@@ -4,23 +4,46 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.joanzapata.iconify.fonts.IoniconsIcons;
 import com.randomappsinc.foodjournal.Fragments.CheckInsFragment;
+import com.randomappsinc.foodjournal.Fragments.DatePickerFragment;
 import com.randomappsinc.foodjournal.Models.CheckIn;
 import com.randomappsinc.foodjournal.Persistence.DatabaseManager;
 import com.randomappsinc.foodjournal.R;
+import com.randomappsinc.foodjournal.Utils.TimeUtils;
 import com.randomappsinc.foodjournal.Utils.UIUtils;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class EditCheckInActivity extends StandardActivity {
 
     public static final String CHECK_IN_KEY = "checkIn";
 
+    private final DatePickerFragment.Listener mDateListener = new DatePickerFragment.Listener() {
+        @Override
+        public void onDateChosen(long dateTimeInMillis) {
+            checkIn.setTimeAdded(dateTimeInMillis);
+            mDateInput.setText(TimeUtils.getDateText(dateTimeInMillis));
+        }
+
+        @Override
+        public long getCurrentTime() {
+            return checkIn.getTimeAdded();
+        }
+    };
+
+    @BindView(R.id.experience_input) EditText mExperienceInput;
+    @BindView(R.id.date_input) TextView mDateInput;
+
     private CheckIn checkIn;
+    private DatePickerFragment mDatePickerFragment;
     private MaterialDialog mDeleteConfirmationDialog;
 
     @Override
@@ -45,6 +68,25 @@ public class EditCheckInActivity extends StandardActivity {
                     }
                 })
                 .build();
+
+        mDatePickerFragment = new DatePickerFragment();
+        mDatePickerFragment.setListener(mDateListener);
+
+        mExperienceInput.setText(checkIn.getMessage());
+        mDateInput.setText(TimeUtils.getDateText(checkIn.getTimeAdded()));
+    }
+
+    @OnClick(R.id.date_input)
+    public void setDate() {
+        mDatePickerFragment.show(getFragmentManager(), "datePicker");
+    }
+
+    @OnClick(R.id.save)
+    public void onCheckInSaved() {
+        checkIn.setMessage(mExperienceInput.getText().toString().trim());
+        DatabaseManager.get().getCheckInsDBManager().updateCheckIn(checkIn);
+        setResult(CheckInsFragment.EDIT_RESULT_CODE);
+        finish();
     }
 
     @Override
