6c58c5f2cfa893b03a2b550e0b16cd7935755e7d
Gear61 <chessnone@yahoo.com>
2017-09-21 00:54:33 -0700

    Add toolbar and close button on full picture

--- app/src/main/java/com/randomappsinc/foodjournal/activities/FullPictureActivity.java
+++ app/src/main/java/com/randomappsinc/foodjournal/activities/FullPictureActivity.java
@@ -3,6 +3,7 @@ package com.randomappsinc.foodjournal.activities;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -14,17 +15,20 @@ import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class FullPictureActivity extends AppCompatActivity{
 
     public static final String IMAGE_URI_KEY = "imageUri";
 
     @BindView(R.id.dish_picture) ImageView mDishPicture;
+    @BindView(R.id.toolbar) View mToolbar;
 
     private final Callback mImageLoadingCallback = new Callback() {
         @Override
         public void onSuccess() {
             mDishPicture.animate().alpha(1.0f).setDuration(getResources().getInteger(R.integer.default_anim_length));
+            mToolbar.animate().alpha(1.0f).setDuration(getResources().getInteger(R.integer.default_anim_length));
         }
 
         @Override
@@ -48,6 +52,11 @@ public class FullPictureActivity extends AppCompatActivity{
                 .fit()
                 .centerCrop()
                 .into(mDishPicture, mImageLoadingCallback);
+    }
+
+    @OnClick(R.id.close)
+    public void closePage() {
+        finish();
     }
 
     @Override
